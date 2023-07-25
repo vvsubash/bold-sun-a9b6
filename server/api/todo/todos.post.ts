450f1ad8d08b44a94c81f6a868bfdd94e1fd9712
venkat <subash.venkat007@gmail.com>
2023-07-25 15:41:53 +0530

    returning message

--- server/api/todo/todos.post.ts
+++ server/api/todo/todos.post.ts
@@ -1,20 +1,18 @@
-import { todos  } from '../../database/schema/todo'
-import { drizzle as drizzleD1, DrizzleD1Database} from "drizzle-orm/d1"
-export default defineEventHandler(async ({context}) => {
-    try {
-        if(context?.cloudflare?.env) {
-            const db:DrizzleD1Database = drizzleD1(context.cloudflare.env.bold_sun)
-            return await db.insert(todos).values(
-                {
-                    id: 3,
-                    task:"Scratch Bunty Back"
-                }
-            )
-        } else {
-          return "Not in cloudflare env"
-        }
-      } catch (error) {
-        throw(error)
-      }
-
-})
+import { todos } from "../../database/schema/todo";
+import { drizzle as drizzleD1, DrizzleD1Database } from "drizzle-orm/d1";
+export default defineEventHandler(({ context }) => {
+  try {
+    if (context?.cloudflare?.env) {
+      const db: DrizzleD1Database = drizzleD1(context.cloudflare.env.bold_sun);
+      db.insert(todos).values({
+        id: 3,
+        task: "Scratch 🐩 Back",
+      });
+      return "data inserted into cloudflare D1 database";
+    } else {
+      return "Not in cloudflare env";
+    }
+  } catch (error) {
+    throw error;
+  }
+});
