4cec2e2a900558943c4502a80fd26169ca1ace94
venkat <subash.venkat007@gmail.com>
2023-07-25 15:58:59 +0530

    lets check it

--- server/api/todo/todos.post.ts
+++ server/api/todo/todos.post.ts
@@ -1,14 +1,14 @@
 import { todos } from "../../database/schema/todo";
 import { drizzle as drizzleD1, DrizzleD1Database } from "drizzle-orm/d1";
-export default defineEventHandler(({ context }) => {
+export default defineEventHandler(async ({ context }) => {
   try {
     if (context?.cloudflare?.env) {
       const db: DrizzleD1Database = drizzleD1(context.cloudflare.env.bold_sun);
-      db.insert(todos).values({
+      let data = await db.insert(todos).values({
         id: 3,
         task: "Scratch 🐩 Back",
-      });
-      return "data inserted into cloudflare D1 database";
+      }).returning().get()
+      return "data inserted into cloudflare D1 database" + data.task;
     } else {
       return "Not in cloudflare env";
     }
