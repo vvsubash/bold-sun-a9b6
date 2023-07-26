76cf49547362c772a7c8bc42bbd6e6ca8128c4f5
venkat <subash.venkat007@gmail.com>
2023-07-26 18:46:21 +0530

    added migrations directory

---
+++ server/database/migrations/0001_dizzy_shiver_man.sql
@@ -0,0 +1,1 @@
+ALTER TABLE todo ADD `priority` integer DEFAULT 1 NOT NULL;
--- server/database/schema/todo.ts
+++ server/database/schema/todo.ts
@@ -2,5 +2,6 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core"
 
 export const todos = sqliteTable('todo', {
     id: integer('id').primaryKey(),
-    task: text('task').notNull()
+    task: text('task').notNull(),
+    priority: integer('priority').notNull().default(1)
 })
